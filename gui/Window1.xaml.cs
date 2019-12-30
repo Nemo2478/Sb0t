6f178e46674df6287b364afdf465e2e4ba98df5f
Nemo2478 <nemoschmidt7@gmail.com>
2019-12-30 10:46:04 +0100

    Add files via upload

---
+++ gui/Window1.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace gui
+{
+    /// <summary>
+    /// Interaktionslogik für Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        String DataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) +
+               "\\sb0t\\" + AppDomain.CurrentDomain.FriendlyName + "\\Scripting\\";
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                List<String> list = new List<String>();
+
+                for (int i = 0; i < tb.LineCount; i++)//tb=textbox
+                {
+                    string text = tb.GetLineText(i).Replace("\r\n","") + ".js";
+                    list.Add(text);
+                }
+                String path = System.IO.Path.Combine(DataPath, "autorun.dat");
+                File.WriteAllLines(path, list.ToArray());
+            }
+            catch { }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.tb.AcceptsReturn = true;
+            String path = System.IO.Path.Combine(DataPath, "autorun.dat");
+            if (File.Exists(path))
+            {
+                String[] lines = File.ReadAllLines(path);
+                foreach (String str in lines)
+                {
+                    if (lines.Last() == str)
+                    {
+                        this.tb.AppendText(str.Replace(".js", ""));
+                    }
+                    else
+                    {
+                        this.tb.AppendText(str.Replace(".js", "") + Environment.NewLine);
+                    }
+                }
+            }
+        }
+    }
+}
